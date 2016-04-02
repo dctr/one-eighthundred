245a134d6696713decad4d5f23687be374c3ea44
David Christ <david.christ@genitopia.org>
2016-04-02 18:07:21 +0200

    Add file reading

--- src/main/java/com/aconex/codingchallenge/oneeighthundred/Constants.java
+++ src/main/java/com/aconex/codingchallenge/oneeighthundred/Constants.java
@@ -1,5 +1,5 @@
 package com.aconex.codingchallenge.oneeighthundred;
 
 public class Constants {
-    public static final String DEFAULT_DICTIONARY_PATH = "en_GB.txt";
+    public static final String DEFAULT_DICTIONARY_PATH = "dictionary.en_GB.txt";
 }
